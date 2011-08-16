61b9a7b9fe5487d52ef17ee5c6680589a261bcdb
Steven Haryanto <stevenharyanto@gmail.com>
2011-08-16 05:16:50 +0000

    Import of SHARYANTO/SHARYANTO-Dummy-Dist2-0.01 from CPAN.
    gitpan-cpan-distribution: SHARYANTO-Dummy-Dist2
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/SHARYANTO-Dummy-Dist2-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/SHARYANTO/Dummy/Dist2.pm
@@ -0,0 +1,34 @@
+package SHARYANTO::Dummy::Dist1;
+BEGIN {
+  $SHARYANTO::Dummy::Dist1::VERSION = '0.01';
+}
+
+# VERSION
+
+1;
+# ABSTRACT: Dummy dist to test PAUSE indexing
+
+__END__
+=pod
+
+=head1 NAME
+
+SHARYANTO::Dummy::Dist1 - Dummy dist to test PAUSE indexing
+
+=head1 VERSION
+
+version 0.01
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/SHARYANTO/Dummy/Module1.pm
@@ -0,0 +1,35 @@
+package SHARYANTO::Dummy::Module1;
+BEGIN {
+  $SHARYANTO::Dummy::Module1::VERSION = '0.01';
+}
+
+sub from_dist2 {
+}
+
+1;
+# ABSTRACT: Dummy dist to test PAUSE indexing, add functions to Module1
+
+__END__
+=pod
+
+=head1 NAME
+
+SHARYANTO::Dummy::Module1 - Dummy dist to test PAUSE indexing, add functions to Module1
+
+=head1 VERSION
+
+version 0.01
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
